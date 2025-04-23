5bd148adcf1fb5f1c40cced69a70cb9408f0504a
mannuh456 <emmanuelmusyox@gmail.com>
2025-04-23 13:08:25 +0300

    cusomizing user model

--- shoppit/core/admin.py
+++ shoppit/core/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+
 
 # Register your models here.
+class CustomUserAdmin(UserAdmin):
+    add_fieldsets =(
+        (None, {
+            'classes':('wide',),
+            'fields':('username','email','first_name','last_name','password','confirm password','city','state','address','phone','is_staff','is_active')
+        })
+    )
+
+admin.site.register(CustomUser,CustomUserAdmin)
